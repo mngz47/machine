3d19ce376ee3809ba836dc3f1618c749d50de682
Matthew <mngz636@gmail.com>
2026-04-30 17:25:37 +0200

    Update campaign.js

--- campaign.js
+++ campaign.js
@@ -12,8 +12,11 @@
 						for(var a=0;a<jsonData.length;a++){
 
              var campaign = jsonData[a];
-           
-e('campaigns').innerHTML += '<p style="animation: 20s slider infinite;" > <h2>'+campaign["title"]+'</h2> '+campaign["description"]+', <b> Audio: <a href='+campaign["audio"]+' >'+campaign["audio"]+'</a> <b> Platform:  '+campaign["platform"]+'  <b> Allow Content: '+campaign["allowed content"]+'</p>';
+
+							
+e('campaigns').innerHTML += '<p style="animation: 20s slider infinite;" > <h2>'+campaign["title"]+'</h2> '+campaign["description"]+', <br> <label class=block >Audio:</label> <a href='+campaign["audio"]+' >'+campaign["audio"]+'</a> <br> <label class=block >Platform:</label>  '+campaign["platform"]+'  <br> <label class=block >Allow Content:</label> '+campaign["allowed content"]+'</p>';
+							e('campaigns').innerHTML += '<a href="https://t.me/share/url?url=https://app.clipster.gg/dashboard/auth?ref_code=mngz44&text='+campaign["title"]+','+campaign["audio"]+','+campaign["allowed content"]+'" target="_blank">Share on Telegram</a>';
+							
 						}
 
 					}

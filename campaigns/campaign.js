dd08295929e9baaad75d6f02bacfc71fad003b94
Matthew <mngz636@gmail.com>
2026-05-06 02:36:51 +0200

    Update campaign.js

--- campaigns/campaign.js
+++ campaigns/campaign.js
@@ -14,9 +14,25 @@
 
              var campaign = jsonData[a];
 
+							var camp_f_web = '<h2>'+campaign["title"]+'</h2> '+campaign["description"]+', <br> <label class=block >Audio:</label> <a href='+campaign["audio"]+' >'+campaign["audio"]+'</a> <br> <label class=block >Platform:</label>  '+campaign["platform"]+'  <br> <label class=block >Allowed Content:</label> '+campaign["allowed content"]+'';
+
+								var camp_f_telegram = campaign["title"]+'\n'+
+									campaign["description"]+
+									'\n Audio: ('+campaign["audio"]+
+									'\n Platform: '+campaign["platform"]+
+									')\n Allowed Content: '+campaign["allowed content"];
+
+							var camp_f_copy = campaign["title"]+'\n'+
+									campaign["description"].replace("<a href=https://app.clipster.gg/dashboard/auth?ref_code=mngz44 >[active]</a>
+","[active](https://app.clipster.gg/dashboard/auth?ref_code=mngz44)
+")+
+									'\n Audio: ('+campaign["audio"]+
+									'\n Platform: '+campaign["platform"]+
+									')\n Allowed Content: '+campaign["allowed content"];
+
 							
-e('campaigns').innerHTML += '<p style="animation: 20s slider infinite;" > <h2>'+campaign["title"]+'</h2> '+campaign["description"]+', <br> <label class=block >Audio:</label> <a href='+campaign["audio"]+' >'+campaign["audio"]+'</a> <br> <label class=block >Platform:</label>  '+campaign["platform"]+'  <br> <label class=block >Allow Content:</label> '+campaign["allowed content"]+'</p>';
-e('campaigns').innerHTML += '<a href="https://t.me/share/url?url=https://bit.ly/48GtBaZ&text='+campaign["title"]+','+campaign["audio"]+','+campaign["allowed content"]+'" target="_blank">Share on Telegram</a>-<a onclick="navigator.clipboard.writeText(parent.innerHTML);return false;" >Copy</a>';
+e('campaigns').innerHTML += '<p style="animation: 20s slider infinite;" >'+camp_f_web+'</p>';
+e('campaigns').innerHTML += '<a href="https://t.me/share/url?url=https://bit.ly/48GtBaZ&text='+camp_f_telegram+'" target="_blank">Share on Telegram</a> - <a onclick="navigator.clipboard.writeText(\''+camp_f_copy+'\');return false;" >Copy</a>';
 							
 						}
 

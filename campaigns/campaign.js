183c7e00165195fb9f6811b0791a3d3ab020a186
Matthew <mngz636@gmail.com>
2026-05-06 02:00:29 +0200

    Update campaign.js

--- campaigns/campaign.js
+++ campaigns/campaign.js
@@ -16,7 +16,7 @@
 
 							
 e('campaigns').innerHTML += '<p style="animation: 20s slider infinite;" > <h2>'+campaign["title"]+'</h2> '+campaign["description"]+', <br> <label class=block >Audio:</label> <a href='+campaign["audio"]+' >'+campaign["audio"]+'</a> <br> <label class=block >Platform:</label>  '+campaign["platform"]+'  <br> <label class=block >Allow Content:</label> '+campaign["allowed content"]+'</p>';
-							e('campaigns').innerHTML += '<a href="https://t.me/share/url?url=https://app.clipster.gg/dashboard/auth?ref_code=mngz44&text='+campaign["title"]+','+campaign["audio"]+','+campaign["allowed content"]+'" target="_blank">Share on Telegram</a>';
+e('campaigns').innerHTML += '<a href="https://t.me/share/url?url=https://bit.ly/48GtBaZ&text='+campaign["title"]+','+campaign["audio"]+','+campaign["allowed content"]+'" target="_blank">Share on Telegram</a>-<a onclick="navigator.clipboard.writeText(parent.innerHTML);return false;" >Copy</a>';
 							
 						}
 
